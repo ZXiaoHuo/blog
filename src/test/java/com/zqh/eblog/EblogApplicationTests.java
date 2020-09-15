9eb33d61753c0cb616b5276552ac206584a72a92
zqh <1468240229@qq.com>
2020-09-15 19:22:09 +0800

    init

---
+++ src/test/java/com/zqh/eblog/EblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zqh.eblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
